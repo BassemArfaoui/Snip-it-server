b0aefe4773f01585ed61f6e438939160fdab886a
BassemArfaoui <bassem.arfaoui@insat.ucar.tn>
2025-12-09 21:57:13 +0100

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_host,
+      port: Number(process.env.DB_port),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD || '',
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
