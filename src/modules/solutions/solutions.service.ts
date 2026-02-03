96183ca10cd0524e2ac0f1e38709fd01baf70881
ilyesbj3 <ilyesbenjemaa17@gmail.com>
2026-02-03 09:15:18 +0100

    some fixes in solution and repository logic

--- src/modules/solutions/solutions.service.ts
+++ src/modules/solutions/solutions.service.ts
@@ -1,257 +1,270 @@
 import {
-  Injectable,
-  NotFoundException,
-  ForbiddenException,
-  BadRequestException,
+    Injectable,
+    NotFoundException,
+    ForbiddenException,
+    BadRequestException,
 } from '@nestjs/common';
-import { DataSource } from 'typeorm';
-import { SolutionRepository } from './repositories/solution.repository';
-import { CreateSolutionDto } from './dto/create-solution.dto';
-import { UpdateSolutionDto } from './dto/update-solution.dto';
-import { Issue } from '../issues/entities/issue.entity';
-import { User } from '../users/entities/user.entity';
-import { Solution } from './entities/solution.entity';
-import { ProfileService } from '../profile/profile.service';
-import { Snippet } from '../snippet/entities/snippet.entity';
+import {DataSource} from 'typeorm';
+import {SolutionRepository} from './repositories/solution.repository';
+import {CreateSolutionDto} from './dto/create-solution.dto';
+import {UpdateSolutionDto} from './dto/update-solution.dto';
+import {Issue} from '../issues/entities/issue.entity';
+import {User} from '../users/entities/user.entity';
+import {Solution} from './entities/solution.entity';
+import {ProfileService} from '../profile/profile.service';
+import {Snippet} from '../snippet/entities/snippet.entity';
 
 @Injectable()
 export class SolutionsService {
-  constructor(
-    private readonly solutionRepo: SolutionRepository,
-    private readonly dataSource: DataSource,
-    private readonly profileService: ProfileService,
-  ) {}
-
-  async create(
-    issueId: number,
-    contributor: User,
-    dto: CreateSolutionDto,
-  ): Promise<Solution> {
-    // Validate that at least one field is provided
-    if (!dto.textContent && !dto.externalLink && !dto.imageUrl && !dto.snippet) {
-      throw new BadRequestException('Either textContent, snippet, externalLink, or imageUrl must be provided');
+    constructor(
+        private readonly solutionRepo: SolutionRepository,
+        private readonly dataSource: DataSource,
+        private readonly profileService: ProfileService,
+    ) {
     }
 
-    return this.dataSource.transaction(async (manager) => {
-      // Verify issue exists
-      const issue = await manager.findOne(Issue, {
-        where: { id: issueId, isDeleted: false },
-        relations: ['user'],
-      });
 
-      if (!issue) {
-        throw new NotFoundException('Issue not found');
-      }
+    async create(
+        issueId: number,
+        contributor: User,
+        dto: CreateSolutionDto,
+    ): Promise<Solution> {
+        if (
+            !dto.textContent &&
+            !dto.externalLink &&
+            !dto.imageUrl &&
+            !dto.snippet
+        ) {
+            throw new BadRequestException(
+                'Either textContent, snippet, externalLink, or imageUrl must be provided',
+            );
+        }
 
-      if (issue.isResolved) {
-        throw new BadRequestException('Issue is already resolved');
-      }
+        const solution = await this.dataSource.transaction(async (manager) => {
+            const issue = await manager.findOne(Issue, {
+                where: {id: issueId, isDeleted: false},
+                relations: ['user'],
+            });
 
-      // Create solution
-      const snippet = dto.snippet
-        ? manager.create(Snippet, {
-            title: dto.snippet.title,
-            content: dto.snippet.content,
-            language: dto.snippet.language,
-          })
-        : undefined;
+            if (!issue) {
+                throw new NotFoundException('Issue not found');
+            }
 
-      const solution = manager.create(Solution, {
-        issue,
-        contributor,
-        textContent: dto.textContent,
-        snippet,
-        externalLink: dto.externalLink,
-        imageUrl: dto.imageUrl,
-      });
+            if (issue.isResolved) {
+                throw new BadRequestException('Issue is already resolved');
+            }
 
-      const savedSolution = await manager.save(solution);
+            let snippet: Snippet | undefined;
 
-      // Increment issue solutions count
-      await manager.increment(
-        Issue,
-        { id: issueId },
-        'solutionsCount',
-        1,
-      );
+            if (dto.snippet) {
+                snippet = manager.create(Snippet, {
+                    title: dto.snippet.title,
+                    content: dto.snippet.content,
+                    language: dto.snippet.language,
+                });
+            }
 
-      // Increment contributor score
-      await manager.increment(
-        User,
-        { id: contributor.id },
-        'contributorScore',
-        1,
-      );
+            const solution = manager.create(Solution, {
+                issue,
+                contributor,
+                textContent: dto.textContent,
+                externalLink: dto.externalLink,
+                imageUrl: dto.imageUrl,
+                snippet,
+            });
 
-      // Increment contributor solutions count
-      await manager.increment(
-        User,
-        { id: contributor.id },
-        'solutionsCount',
-        1,
-      );
+            const savedSolution = await manager.save(solution);
 
-      return savedSolution;
-    }).then(async (solution) => {
-      // Recalculate contributor score after transaction completes
-      await this.profileService.calculateAndPersistScore(contributor.id).catch(() => {
-        // Ignore errors in score calculation
-      });
+            await manager.increment(
+                Issue,
+                {id: issue.id},
+                'solutionsCount',
+                1,
+            );
 
-      // TODO: Send notification to issue owner
-      // await this.notificationService.notifyNewSolution(issue.user.id, solution);
+            await manager.increment(
+                User,
+                {id: contributor.id},
+                'solutionsCount',
+                1,
+            );
 
-      return solution;
-    });
-  }
+            await manager.increment(
+                User,
+                {id: contributor.id},
+                'contributorScore',
+                1,
+            );
 
-  async findByIssue(issueId: number): Promise<Solution[]> {
-    return this.solutionRepo.findByIssue(issueId);
-  }
+            return savedSolution;
+        });
 
-  async findOne(solutionId: number): Promise<Solution> {
-    const solution = await this.solutionRepo.findById(solutionId);
-    
-    if (!solution) {
-      throw new NotFoundException('Solution not found');
+
+        this.profileService
+            .calculateAndPersistScore(contributor.id)
+            .catch(() => {
+            });
+
+        return solution;
     }
 
-    return solution;
-  }
-
-  async update(
-    solutionId: number,
-    user: User,
-    dto: UpdateSolutionDto,
-  ): Promise<Solution> {
-    const solution = await this.findOne(solutionId);
-
-    if (solution.contributor.id !== user.id) {
-      throw new ForbiddenException('Only the contributor can update this solution');
+    async findByIssue(issueId: number): Promise<Solution[]> {
+        return this.solutionRepo.findByIssue(issueId);
     }
 
-    if (solution.isAccepted) {
-      throw new BadRequestException('Cannot update an accepted solution');
+    async findOne(solutionId: number): Promise<Solution> {
+        const solution = await this.solutionRepo.findActiveById(solutionId);
+
+        if (!solution) {
+            throw new NotFoundException('Solution not found');
+        }
+
+        return solution;
     }
 
-    const { snippet, ...rest } = dto;
-    Object.assign(solution, rest);
 
-    if (snippet) {
-      if (solution.snippet) {
-        Object.assign(solution.snippet, snippet);
-      } else {
-        const newSnippet = new Snippet();
-        Object.assign(newSnippet, snippet);
-        solution.snippet = newSnippet;
-      }
+    async update(
+        solutionId: number,
+        user: User,
+        dto: UpdateSolutionDto,
+    ): Promise<Solution> {
+        const solution = await this.findOne(solutionId);
+
+        if (solution.contributor.id !== user.id) {
+            throw new ForbiddenException(
+                'Only the contributor can update this solution',
+            );
+        }
+
+        if (solution.isAccepted) {
+            throw new BadRequestException(
+                'Cannot update an accepted solution',
+            );
+        }
+
+        const {snippet, ...rest} = dto;
+        Object.assign(solution, rest);
+
+        if (snippet) {
+            if (solution.snippet) {
+                Object.assign(solution.snippet, snippet);
+            } else {
+                solution.snippet = Object.assign(new Snippet(), snippet);
+            }
+        }
+
+        return this.solutionRepo.save(solution);
     }
-    return this.solutionRepo.repo.save(solution);
-  }
 
-  async delete(solutionId: number, user: User): Promise<void> {
-    return this.dataSource.transaction(async (manager) => {
-      const solution = await manager.findOne(Solution, {
-        where: { id: solutionId, isDeleted: false },
-        relations: ['contributor', 'issue'],
-      });
 
-      if (!solution) {
-        throw new NotFoundException('Solution not found');
-      }
+    async delete(solutionId: number, user: User): Promise<void> {
+        await this.dataSource.transaction(async (manager) => {
+            const solution =
+                await this.solutionRepo.findActiveById(solutionId, manager);
 
-      if (solution.contributor.id !== user.id) {
-        throw new ForbiddenException('Only the contributor can delete this solution');
-      }
+            if (!solution) {
+                throw new NotFoundException('Solution not found');
+            }
 
-      if (solution.isAccepted) {
-        throw new BadRequestException('Cannot delete an accepted solution');
-      }
+            if (solution.contributor.id !== user.id) {
+                throw new ForbiddenException(
+                    'Only the contributor can delete this solution',
+                );
+            }
 
-      // Soft delete solution
-      await manager.update(Solution, { id: solutionId }, { isDeleted: true });
+            if (solution.isAccepted) {
+                throw new BadRequestException(
+                    'Cannot delete an accepted solution',
+                );
+            }
 
-      // Decrement issue solutions count
-      await manager.decrement(
-        Issue,
-        { id: solution.issue.id },
-        'solutionsCount',
-        1,
-      );
+            await this.solutionRepo.softDelete(solutionId, manager);
 
-      // Decrement contributor score
-      await manager.decrement(
-        User,
-        { id: solution.contributor.id },
-        'contributorScore',
-        1,
-      );
+            await manager.decrement(
+                Issue,
+                {id: solution.issue.id},
+                'solutionsCount',
+                1,
+            );
 
-      // Decrement contributor solutions count
-      await manager.decrement(
-        User,
-        { id: solution.contributor.id },
-        'solutionsCount',
-        1,
-      );
-    }).then(async () => {
-      // Recalculate contributor score after transaction completes
-      await this.profileService.calculateAndPersistScore(user.id).catch(() => {
-        // Ignore errors in score calculation
-      });
-    });
-  }
+            await manager.decrement(
+                User,
+                {id: solution.contributor.id},
+                'solutionsCount',
+                1,
+            );
 
-  async acceptSolution(solutionId: number, user: User): Promise<Solution> {
-    return this.dataSource.transaction(async (manager) => {
-      const solution = await manager.findOne(Solution, {
-        where: { id: solutionId, isDeleted: false },
-        relations: ['contributor', 'issue', 'issue.user', 'snippet'],
-      });
+            await manager.decrement(
+                User,
+                {id: solution.contributor.id},
+                'contributorScore',
+                1,
+            );
+        });
 
-      if (!solution) {
-        throw new NotFoundException('Solution not found');
-      }
+        this.profileService
+            .calculateAndPersistScore(user.id)
+            .catch(() => {
+            });
+    }
 
-      // Only issue owner can accept solution
-      if (solution.issue.user.id !== user.id) {
-        throw new ForbiddenException('Only the issue owner can accept a solution');
-      }
 
-      if (solution.issue.isResolved) {
-        throw new BadRequestException('Issue is already resolved');
-      }
+    async acceptSolution(
+        solutionId: number,
+        user: User,
+    ): Promise<Solution> {
+        const solution = await this.dataSource.transaction(async (manager) => {
+            const solution =
+                await this.solutionRepo.findActiveById(solutionId, manager);
 
-      if (solution.isAccepted) {
-        throw new BadRequestException('Solution is already accepted');
-      }
+            if (!solution) {
+                throw new NotFoundException('Solution not found');
+            }
 
-      // Mark solution as accepted
-      await manager.update(Solution, { id: solutionId }, { isAccepted: true });
+            if (solution.issue.user.id !== user.id) {
+                throw new ForbiddenException(
+                    'Only the issue owner can accept a solution',
+                );
+            }
 
-      // Mark issue as resolved
-      await manager.update(Issue, { id: solution.issue.id }, { isResolved: true });
+            if (solution.issue.isResolved) {
+                throw new BadRequestException('Issue is already resolved');
+            }
 
-      // Bonus reward for contributor (additional points)
-      await manager.increment(
-        User,
-        { id: solution.contributor.id },
-        'contributorScore',
-        5, // Bonus points for accepted solution
-      );
+            if (solution.isAccepted) {
+                throw new BadRequestException(
+                    'Solution is already accepted',
+                );
+            }
 
-      solution.isAccepted = true;
-      return solution;
-    }).then(async (solution) => {
-      // Recalculate contributor score after transaction completes
-      await this.profileService.calculateAndPersistScore(solution.contributor.id).catch(() => {
-        // Ignore errors in score calculation
-      });
+            await this.solutionRepo.markAsAccepted(
+                solutionId,
+                manager,
+            );
 
-      // TODO: Send notification to contributor
-      // await this.notificationService.notifySolutionAccepted(solution.contributor.id, solution);
+            await manager.update(
+                Issue,
+                {id: solution.issue.id},
+                {isResolved: true},
+            );
 
-      return solution;
-    });
-  }
+            await manager.increment(
+                User,
+                {id: solution.contributor.id},
+                'contributorScore',
+                5,
+            );
+
+            solution.isAccepted = true;
+            return solution;
+        });
+
+        this.profileService
+            .calculateAndPersistScore(solution.contributor.id)
+            .catch(() => {
+            });
+
+        return solution;
+    }
 }
+
